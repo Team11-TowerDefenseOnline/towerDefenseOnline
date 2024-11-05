50ceb84856b147f3b686da2e2a411bd9b58c67a7
vxbdfsg <39340989+vxbdfsg@users.noreply.github.com>
2024-11-05 12:56:30 +0900

    [update] [onData 패킷 파서 수정] [ 커먼프로토 추가 ] [ config파일 수정 ]
    [update] [onData 패킷 파서 수정] [ 커먼프로토 추가 ] [ config파일 수정 ]

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 import { PORT, HOST, CLIENT_VERSION } from '../constants/env.js';
-import { TOTAL_LENGTH, PACKET_TYPE_LENGTH, PACKET_TYPE } from '../constants/header.js';
+import { PACKET_TYPE_LENGTH, PACKET_TYPE, PAYLOAD_LENGTH, VERSION_LENGTH, SEQUENCE } from '../constants/header.js';
 import {
   DB_NAME,
   DB_USER,
@@ -17,13 +17,47 @@ export const config = {
     version: CLIENT_VERSION,
   },
   packet: {
-    totalLength: TOTAL_LENGTH,
-    typeLength: PACKET_TYPE_LENGTH,
+    payloadLength: PAYLOAD_LENGTH,
+    packetTypeLength: PACKET_TYPE_LENGTH,
+    versionLength : VERSION_LENGTH,
+    sequenceLength : SEQUENCE,
+    totalLength : PACKET_TYPE_LENGTH + PAYLOAD_LENGTH + VERSION_LENGTH + SEQUENCE
   },
   packetType: {
+    /*
     ping: PACKET_TYPE.PING,
     nomal: PACKET_TYPE.NORMAL,
     location: PACKET_TYPE.LOCATION,
+    */
+    registerRequest: PACKET_TYPE.REGISTER_REQUEST,
+    registerResponse: PACKET_TYPE.REGISTER_RESPONSE,
+    loginRequest: PACKET_TYPE.LOGIN_REQUEST,
+    loginResponse: PACKET_TYPE.LOGIN_RESPONSE,
+
+    matchRequest: PACKET_TYPE.MATCH_REQUEST,
+    matchStartNotification: PACKET_TYPE.MATCH_START_NOTIFICATION,
+
+    stateSyncNotification: PACKET_TYPE.STATE_SYNC_NOTIFICATION,
+
+    towerPurchaseRequest: PACKET_TYPE.TOWER_PURCHASE_REQUEST,
+    towerPurchaseResponse: PACKET_TYPE.TOWER_PURCHASE_RESPONSE,
+    addEnemyTowerNotification: PACKET_TYPE.ADD_ENEMY_TOWER_NOTIFICATION,
+
+    spawnMonsterRequest: PACKET_TYPE.SPAWN_MONSTER_REQUEST,
+    spawnMonsterResponse: PACKET_TYPE.SPAWN_MONSTER_RESPONSE,
+    spawnEnemyMonsterNotification: PACKET_TYPE.SPAWN_ENEMY_MONSTER_NOTIFICATION,
+
+    towerAttackRequest: PACKET_TYPE.TOWER_ATTACK_REQUEST,
+    enemyTowerAttackNotification: PACKET_TYPE.ENEMY_TOWER_ATTACK_NOTIFICATION,
+    monsterAttackBaseRequest: PACKET_TYPE.MONSTER_ATTACK_BASE_REQUEST,
+
+    updateBaseHpNotification: PACKET_TYPE.UPDATE_BASE_HP_NOTIFICATION,
+    gameOverNotification: PACKET_TYPE.GAME_OVER_NOTIFICATION,
+
+    gameEndRequest: PACKET_TYPE.GAME_END_REQUEST,
+
+    monsterDeathNotification: PACKET_TYPE.MONSTER_DEATH_NOTIFICATION,
+    enemyMonsterDeathNotification: PACKET_TYPE.ENEMY_MONSTER_DEATH_NOTIFICATION,
   },
   database: {
     database: DB_NAME,
--- src/protobuf/packetName.js
+++ src/protobuf/packetName.js
@@ -2,6 +2,10 @@ export const packetNames = {
   common: {
     Packet: "common.Packet",
     Ping: "common.Ping",
+    C2SRegisterRequest : "common.C2SRegisterRequest",
+    S2CRegisterResponse: "common.S2CRegisterResponse",
+    C2SLoginRequest : "common.C2SLoginRequest",
+    S2CLoginResponse : "common.S2CLoginResponse"
   },
   response: {
     Response: "response.Response",
