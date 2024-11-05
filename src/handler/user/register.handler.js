329c21fe4f5a1224e0faf7860960c82c554aee28
skybanana <112135946+skybanana@users.noreply.github.com>
2024-11-05 19:58:58 +0900

    [update] [register.handler]

--- src/handler/user/register.handler.js
+++ src/handler/user/register.handler.js
@@ -1,23 +1,38 @@
 import { createUser } from "../../db/user/user.db.js";
 import { getProtoMessages } from "../../init/loadProto.js";
+import { serializer } from "../../utils/notification/game.notification.js";
 
-const registerHandler = (socket, payload) => {
+const registerHandler = async ({socket, payloadData}) => {
     const protoMessages = getProtoMessages();
-    const Register = protoMessages.common.C2SRegisterRequest;
+    const request = protoMessages.common.C2SRegisterRequest;
     let sendPayload;
 
     try {
-        const registerMessage = Register.decode(payloadData.subarray(2));
-        const {id, password, email} = registerMessage;
-        createUser(id, password, email);
+        const requestMessage = request.decode(payloadData.subarray(2));
+        console.log(requestMessage)
+        const {id, password, email} = requestMessage;
+        // createUser(id, password, email);
+        
+        sendPayload = {
+            success: true, 
+            message: "회원가입 성공!", 
+            failCode: 0};
+
     } catch(error) {
         console.error(`회원가입 실패: ${error}`)
+
+        sendPayload = {
+            success: false, 
+            message: "회원가입 실패!", 
+            failCode: 2};
     }
 
-    const message = Register.create({success: true, message: "회원가입 성공!", failCode: new GlobalFailCode()});
-    const Packet = Register.encode(message).finish();
+    const response = protoMessages.common.S2CRegisterResponse;
+    const message = response.create(sendPayload);
+    const packet = response.encode(message).finish();
+    console.log(response.decode(packet))
 
-    socket.write()
+    socket.write(serializer(packet, 2, 1));
 }
 
 export default registerHandler;
--- src/utils/notification/game.notification.js
+++ src/utils/notification/game.notification.js
@@ -1,19 +1,25 @@
 import { config } from "../../config/config.js";
 import { getProtoMessages } from "../../init/loadProto.js";
 
-const serializer = (message, type) => {
-  const packetLength = Buffer.alloc(config.packet.totalLength);
-  packetLength.writeUInt32BE(
-    message.length + config.packet.totalLength + config.packet.typeLength,
-    0
-  ); // 패킷 길이에 타입 바이트 포함
-
+export const serializer = (message, type, sequenceData) => {
   // 패킷 타입 정보를 포함한 버퍼 생성
-  const packetType = Buffer.alloc(config.packet.typeLength);
-  packetType.writeUInt8(type, 0);
+  const packetType = Buffer.alloc(config.packet.packetTypeLength);
+  packetType.writeUIntBE(type, 0, config.packet.packetTypeLength);
+
+  const versionLength = Buffer.alloc(config.packet.versionLength);
+  versionLength.writeUintBE(config.client.version.length, 0, config.packet.versionLength);
+
+  const version = Buffer.alloc(config.client.version.length);
+  version.write(config.client.version, 0, config.client.version.length);
+
+  const sequence = Buffer.alloc(config.packet.sequenceLength);
+  sequence.writeUintBE(sequenceData+1, 0, config.packet.sequenceLength);
+
+  const payloadLength = Buffer.alloc(config.packet.payloadLength);
+  payloadLength.writeUintBE(message.length, 0, config.packet.payloadLength);
 
   // 길이 정보와 메시지를 함께 전송
-  return Buffer.concat([packetLength, packetType, message]);
+  return Buffer.concat([packetType, versionLength, version, sequence, payloadLength, message]);
 };
 
 export const createLocationPacket = (users) => {
