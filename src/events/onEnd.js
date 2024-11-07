330779e53b59755e5187186b1972b1229c1ea3d9
skybanana <112135946+skybanana@users.noreply.github.com>
2024-11-07 11:25:22 +0900

    [fix] [onEnd 이벤트 에러]

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,4 +1,4 @@
-import { getGameSession } from '../session/game.session.js';
+import { getGameSession, removeGameSession } from '../session/game.session.js';
 import { removeUser } from '../session/user.session.js';
 
 // 클라이언트 종료시 작동하는 이벤트
@@ -6,9 +6,10 @@ import { removeUser } from '../session/user.session.js';
 // try catch 넣으면 강제 종료시 에러가 발생함.
 export const onEnd = (socket) => async () => {
   console.log('클라이언트 연결이 종료되었습니다.');
-  
+
   await removeUser(socket);
 
-  const gameSession = getGameSession();
-  gameSession.removeUser(socket);
+  // 클라이언트가 참가 중이던 게임 세션을 제거해야함.
+  // 상대가 탈주해서 세션이 제거된 경우 상대에게 상대가 탈주함을 알리고 메인화면으로 보내줘야함.
+  // removeGameSession(socket.id);
 };
